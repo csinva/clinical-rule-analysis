0382420e66cfcf5105148fa707ece4260a4cdf42
Chandan Singh <csinva@yahoo.com>
2022-08-02 13:59:25 -0700

    Updates from Overleaf

--- viz.py
+++
@@ -1,8 +0,0 @@
-from os.path import join as oj
-
-import matplotlib.pyplot as plt
-
-
-def savefig(fname):
-    plt.savefig(oj('results', fname + '.png'), dpi=300)
-    plt.savefig(oj('results', fname + '.pdf'))
