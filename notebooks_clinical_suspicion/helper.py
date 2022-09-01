a08077ab6a237ccf47274fafedbc507617e0e382
Chandan Singh <chandan_singh@berkeley.edu>
2022-09-01 09:20:15 -0700

    add naive avg prediction plot

--- notebooks_clinical_suspicion/helper.py
+++ notebooks_clinical_suspicion/helper.py
@@ -1,4 +1,14 @@
 import numpy as np
+import warnings
+from os.path import join as oj
+import sys
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from sklearn import metrics
+import sklearn.metrics
+
 
 def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
     '''Predict each subgroup in the original PECARN rule
@@ -12,7 +22,7 @@ def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
         ('AbdTrauma_or_SeatBeltSign', ['yes']),
         ('GCSScore', range(14)),
         ('AbdTenderDegree', ['Mild', 'Moderate', 'Severe']),
-        ('ThoracicTrauma', ['yes']),        
+        ('ThoracicTrauma', ['yes']),
         ('AbdomenPain', ['yes']),
         ('DecrBreathSound', ['yes']),
         ('VomitWretch', ['yes']),
@@ -23,9 +33,11 @@ def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
     for rule in rules:
         k, vals = rule
         preds_high_risk_new = d[k].isin(vals)
-        idxs_sub_category = preds_high_risk_new & (~preds_high_risk) # this is the new cohort of patients that is high risk
-        preds_probabilistic[idxs_sub_category] = np.mean(d[idxs_sub_category][o])
-        preds_high_risk = preds_high_risk | preds_high_risk_new 
+        # this is the new cohort of patients that is high risk
+        idxs_sub_category = preds_high_risk_new & (~preds_high_risk)
+        preds_probabilistic[idxs_sub_category] = np.mean(
+            d[idxs_sub_category][o])
+        preds_high_risk = preds_high_risk | preds_high_risk_new
 
         if verbose:
             idxs_high_risk = d_small[k].isin(vals)
@@ -33,7 +45,37 @@ def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
             d_small = d_small[~idxs_high_risk]
             num2 = do[o].sum()
             denom2 = do.shape[0]
-            print(f'{k:<25} {d_small[o].sum():>3} / {d_small.shape[0]:>5}\t{num2:>3} / {denom2:>4} ({num2/denom2*100:0.1f})')
-    preds_probabilistic[~preds_high_risk] = np.mean(d[~preds_high_risk][o])        
+            print(
+                f'{k:<25} {d_small[o].sum():>3} / {d_small.shape[0]:>5}\t{num2:>3} / {denom2:>4} ({num2/denom2*100:0.1f})')
+    preds_probabilistic[~preds_high_risk] = np.mean(d[~preds_high_risk][o])
     return preds_probabilistic, preds_high_risk.astype(int)
-    
+
+
+def all_stats_curve(y_test, preds_proba, plot=False, thresholds=None):
+    '''preds_proba should be 1d
+    '''
+    if thresholds is None:
+        thresholds = sorted(np.unique(preds_proba))
+    all_stats = {
+        s: [] for s in ['sens', 'spec', 'ppv', 'npv', 'lr+', 'lr-']
+    }
+    for threshold in thresholds:
+        preds = preds_proba > threshold
+        tn, fp, fn, tp = metrics.confusion_matrix(y_test, preds).ravel()
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            sens = tp / (tp + fn)
+            spec = tn / (tn + fp)
+            all_stats['sens'].append(sens)
+            all_stats['spec'].append(spec)
+            all_stats['ppv'].append(tp / (tp + fp))
+            all_stats['npv'].append(tn / (tn + fn))
+            all_stats['lr+'].append(sens / (1 - spec))
+            all_stats['lr-'].append((1 - sens) / spec)
+
+    if plot:
+        plt.plot(all_stats['sens'], all_stats['spec'], '.-')
+        plt.xlabel('sensitivity')
+        plt.ylabel('specificity')
+        plt.grid()
+    return all_stats, thresholds
