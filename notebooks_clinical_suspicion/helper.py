2c75c128838b9441035c77455ee2f7a20fb555de
Chandan Singh <chandan_singh@berkeley.edu>
2022-09-01 08:37:53 -0700

    return ai prob. preds

--- notebooks_clinical_suspicion/helper.py
+++ notebooks_clinical_suspicion/helper.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
     '''Predict each subgroup in the original PECARN rule
-    Return the predictions of the PECARN rule
+    Return the probabilistic predictions of the PECARN rule and the threshold binary predictions
     '''
     n = d.shape[0]
     npos = d[o].sum()
@@ -18,10 +18,13 @@ def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
         ('VomitWretch', ['yes']),
     ]
     preds_high_risk = np.zeros(n).astype(bool)
+    preds_probabilistic = np.zeros(n)
     d_small = d
     for rule in rules:
         k, vals = rule
         preds_high_risk_new = d[k].isin(vals)
+        idxs_sub_category = preds_high_risk_new & (~preds_high_risk) # this is the new cohort of patients that is high risk
+        preds_probabilistic[idxs_sub_category] = np.mean(d[idxs_sub_category][o])
         preds_high_risk = preds_high_risk | preds_high_risk_new 
 
         if verbose:
@@ -31,5 +34,6 @@ def pecarn_rule_predict(d, o='iai_intervention', verbose=False):
             num2 = do[o].sum()
             denom2 = do.shape[0]
             print(f'{k:<25} {d_small[o].sum():>3} / {d_small.shape[0]:>5}\t{num2:>3} / {denom2:>4} ({num2/denom2*100:0.1f})')
-    return preds_high_risk
+    preds_probabilistic[~preds_high_risk] = np.mean(d[~preds_high_risk][o])        
+    return preds_probabilistic, preds_high_risk.astype(int)
     
